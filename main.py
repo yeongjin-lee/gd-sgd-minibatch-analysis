26c431b5e11b19529e3eb725449830abb4d941f8
Yeongjin Lee <107300480+yeongjin-lee@users.noreply.github.com>
2026-01-13 17:22:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from experiments.exp1_loss_reduction import run_exp1
+from experiments.exp1_loss_decrease import run_exp1
 from experiments.exp2_noise_vs_batch import run_exp2
 
 if __name__ == "__main__":
